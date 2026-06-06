92b70f864c09f8f111a9b56eb4e8a09589099892
Abhishek  Naik <abhi01naik@gmail.com>
2026-06-06 20:40:42 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w7-ay(u+!c2yok(71$f(9h)fg0u(eiz=1=q*kgfd(1qo$qp@e#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [localhost', '127.0.0.1', 'ai-news-summarizer-gp31.onrender.com]
 
